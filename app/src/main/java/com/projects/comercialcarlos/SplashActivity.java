9ad5110a50c40f270ff8e4a9ec0fb9d9323e7d42
Baru Miranda <bmirandas@canvia.com>
2022-11-29 10:32:47 -0500

    feat: avance MainActivity

---
+++ app/src/main/java/com/projects/comercialcarlos/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.projects.comercialcarlos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long DURACION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new CountDownTimer(DURACION_SPLASH,1000){
+            @Override
+            public void onTick(long l) {
+
+            }
+            @Override
+            public void onFinish() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }.start();
+
+    }
+
+}
---
+++ app/src/main/java/com/projects/comercialcarlos/service/FragmentService.java
@@ -0,0 +1,6 @@
+package com.projects.comercialcarlos.service;
+
+public interface FragmentService {
+
+}
+
