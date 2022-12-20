bcfd962c4b89396a1df0ff5d1ab2e37f8c8aa582
Baru Miranda <bmirandas@canvia.com>
2022-12-19 23:25:43 -0500

    feat: implementation de interfaces

---
+++ app/src/main/java/com/projects/comercialcarlos/usuarios/ClientesFragment.java
@@ -0,0 +1,37 @@
+package com.projects.comercialcarlos.usuarios;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.projects.comercialcarlos.R;
+import com.projects.comercialcarlos.activity.MainActivity;
+import com.projects.comercialcarlos.databinding.FragmentClientesBinding;
+import com.projects.comercialcarlos.databinding.FragmentVentasBinding;
+
+
+public class ClientesFragment extends Fragment {
+
+
+    private FragmentClientesBinding binding;
+
+    public static ClientesFragment newInstance() {
+        return new ClientesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentClientesBinding.inflate(inflater, container, false);
+        regionSetup();
+        ((MainActivity) getActivity()).iniView("Clientes -  Tienda Carlos");
+        return binding.getRoot();
+    }
+
+    private void regionSetup() {
+    }
+}
